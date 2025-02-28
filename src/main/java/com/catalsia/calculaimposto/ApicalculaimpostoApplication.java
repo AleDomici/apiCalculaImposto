717a01bee03317e1328c29f11b4b83ddb6325edd
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-02-28 15:28:25 -0300

    first commit

---
+++ src/main/java/com/catalsia/calculaimposto/ApicalculaimpostoApplication.java
@@ -0,0 +1,13 @@
+package com.catalsia.calculaimposto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicalculaimpostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicalculaimpostoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalsia/calculaimposto/ApicalculaimpostoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalsia.calculaimposto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicalculaimpostoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
